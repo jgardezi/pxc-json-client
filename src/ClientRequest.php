d3357c3b733f3e1f416c766aef687b92dbb139ae
quyenhoang <quyenhoang664@gmail.com>
2019-01-29 12:22:12 +0700

    Merge headers

--- src/ClientRequest.php
+++ src/ClientRequest.php
@@ -14,9 +14,11 @@ class ClientRequest extends BasicRequest implements ClientRequestInterface
     function getHeader(bool $public): array
     {
         if ($public) {
-            return [];
+            return $this->headers;
         }
-        return ['Authorization' => "Bearer " . $token = Request::session()->get('pxc.token')];
+        return array_merge(
+            ['Authorization' => "Bearer " . $token = Request::get('pxc.token')], $this->headers
+        );
     }
 
 
